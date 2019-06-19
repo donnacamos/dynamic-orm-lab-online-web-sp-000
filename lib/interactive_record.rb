0ab3f844db4050ce96d618b9930c9cbf7513e797
Donna Amos <donnacamos.developer@gmail.com>
2019-06-19 20:31:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,22 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
+
+  def self.table_names 
+    self.to_s.downcase.pluralize
+  end 
   
+  def self.column_names
+    column_names = [] 
+    
+    sql = "PRAGMA table_info('#{table_names}')" 
+    table_info = DB[:conn].execute(sql) 
+    
+    table_info.each do |col| 
+      column_names << col["name"] 
+    end 
+    column_names.compact 
+  end 
 end
